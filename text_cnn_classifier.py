51b2bb6235e46bdf1ea730f0b983484a6cfc309d
DESKTOP-PK9B0IA\Hyuk <rlagur9286@naver.com>
2017-11-19 21:31:32 +0900

    2017-11-19, H, Evaluate 추가 (# 0.85 정확도)

--- text_cnn_classifier.py
+++ text_cnn_classifier.py
@@ -45,6 +45,11 @@ tf.flags.DEFINE_integer("batch_size", 128, "Batch Size (default: 64)")
 tf.flags.DEFINE_integer("num_epochs", 1000, "Number of training epochs (default: 200)")
 tf.flags.DEFINE_integer("evaluate_every", 100, "Evaluate model on dev set after this many steps (default: 100)")
 tf.flags.DEFINE_integer("checkpoint_every", 100, "Save model after this many steps (default: 100)")
+
+# Eval Parameters
+tf.flags.DEFINE_string("checkpoint_dir", "", "Checkpoint directory from training run")
+tf.flags.DEFINE_boolean("eval_train", False, "Evaluate on all training data")
+
 # Misc Parameters
 tf.flags.DEFINE_boolean("allow_soft_placement", True, "Allow device soft device placement")
 tf.flags.DEFINE_boolean("log_device_placement", False, "Log placement of ops on devices")
@@ -312,7 +317,68 @@ def train():
                         path = saver.save(sess, checkpoint_prefix, global_step=current_step)
                         print("Saved model checkpoint to {}\n".format(path))
 
+
+def eval():
+    print("\nParameters:")
+    for attr, value in sorted(FLAGS.__flags.items()):
+        print("{}={}".format(attr.upper(), value))
+    print("")
+    if FLAGS.eval_train:
+        x_raw, y_test = data_loader.load_data_and_labels()
+        y_test = np.argmax(y_test, axis=1)
+    else:
+        x_raw, y_test = data_loader.load_dev_data_and_labels()
+        y_test = np.argmax(y_test, axis=1)
+
+    # checkpoint_dir이 없다면 가장 최근 dir 추출하여 셋팅
+    if FLAGS.checkpoint_dir == "":
+        all_subdirs = ["./runs/" + d for d in os.listdir('./runs/.') if os.path.isdir("./runs/" + d)]
+        latest_subdir = max(all_subdirs, key=os.path.getmtime)
+        FLAGS.checkpoint_dir = latest_subdir + "/checkpoint"
+
+    # Map data into vocabulary
+    vocab_path = os.path.join(FLAGS.checkpoint_dir, "..", "vocab")
+    vocab_processor = data_loader.restore_vocab_processor(vocab_path)
+    x_test = np.array(list(vocab_processor.transform(x_raw)))
+
+    print("\nEvaluating...\n")
+
+    graph = tf.Graph()
+    with graph.as_default():
+        session_conf = tf.ConfigProto(
+            allow_soft_placement=FLAGS.allow_soft_placement,
+            log_device_placement=FLAGS.log_device_placement)
+        sess = tf.Session(config=session_conf)
+        sess.run(tf.global_variables_initializer())
+
+        checkpoint_file = tf.train.latest_checkpoint(FLAGS.checkpoint_dir)
+        # Load the saved meta graph and restore variables
+        saver = tf.train.import_meta_graph("{}.meta".format(checkpoint_file))
+        saver.restore(sess, checkpoint_file)
+
+        # Get the placeholders from the graph by name
+        input_x = graph.get_operation_by_name("input_x").outputs[0]
+        # input_y = graph.get_operation_by_name("input_y").outputs[0]
+        dropout_keep_prob = graph.get_operation_by_name("dropout_keep_prob").outputs[0]
+
+        # Tensors we want to evaluate
+        predictions = graph.get_operation_by_name("output/predictions").outputs[0]
+
+        # Generate batches for one epoch
+        batches = batch_iter(list(x_test), FLAGS.batch_size, 1, shuffle=False)
+
+        # Collect the predictions here
+        all_predictions = []
+
+        for x_test_batch in batches:
+            batch_predictions = sess.run(predictions, {input_x: x_test_batch, dropout_keep_prob: 1.0})
+            all_predictions = np.concatenate([all_predictions, batch_predictions])
+
+        correct_predictions = float(sum(all_predictions == y_test))
+        print("checkpoint - Accuracy: {:g}".format(correct_predictions / float(len(y_test))))
+
 if __name__ == '__main__':
     # DB로부터 읽어와서
-    read_data()
-    train()
+    # read_data()
+    # train()
+    eval()
